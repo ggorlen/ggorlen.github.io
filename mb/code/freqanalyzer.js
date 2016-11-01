85a8cf3f276626ac6114dce0486e64ec4428de27
ggorlen <gsgorlen@gmail.com>
2016-11-01 13:20:20 -0700

    general updates/refactoring code

--- mb/code/freqanalyzer.js
+++ mb/code/freqanalyzer.js
@@ -1,23 +1,28 @@
 function fAnalyze(words) {
     
+    // create an array from the string parameter, delimited by empty spaces
     words = words.split(" ");
     
+    // create a histogram to count the frequency of each word
     var hist = {};
     
     for (var i = 0; i < words.length; i++) {
         if (words[i] in hist) {
-            hist[words[i]]++;
+            hist[words[i]]++; // increment the count for that word by one
         }
         else {
             hist[words[i]] = 1;
         }
     }
-    return hist;   
+    return hist;   // return our histogram from the function
 }
 
-function main() {
-    var result = fAnalyze("hello, world");
+function main(input) {
+    
+    // we can change this string literal to input variable to get input from a user
+    var result = fAnalyze(input);
 
+    // enhanced for loop to print the result from the dictionary object
     for (var key in result) {
         if (result.hasOwnProperty(key)) {
             console.log(key + ": " + result[key]);
@@ -27,4 +32,4 @@ function main() {
     // how can we sort the output?
 }
 
-main();
+main("hello, world");
