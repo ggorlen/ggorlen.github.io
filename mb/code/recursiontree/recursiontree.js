0fbedb72b5ff3932cc5c5664dc00f6a7f678099d
ggorlen <gsgorlen@gmail.com>
2016-12-15 09:21:52 -0800

    adjust behavior and fix var bug

--- mb/code/recursiontree/recursiontree.js
+++ mb/code/recursiontree/recursiontree.js
@@ -5,36 +5,48 @@
 var canvas = document.getElementById("treecanvas");
 var ctx = canvas.getContext("2d");
 
-var drawLine = function(x, y, distance, angle, angleFactor, frameRate) {
+var drawLine = function(x, y, distance, angle, delay) {
     
-    if (distance > 0) {
+    // end the tree drawing when distance is a negative number
+    if (distance >= 0) {
     
+        // start drawing
         ctx.beginPath();
         ctx.moveTo(x, y);
         
+        // update the x and y coordinates
         x += angle;
         y -= distance;
         
+        // move the pen and finish the line
         ctx.lineTo(x, y);
         ctx.stroke();
         
-        frameRateLeft = frameRate + Math.floor(Math.random() * 100000);
-        frameRateRight = frameRate + Math.floor(Math.random() * 100000);
+        // update delay times for each branch
+        delayLeft = rand(200, 8000);
+        delayRight = rand(200, 8000);
 
-        // (if distance % 2 === 0 ). ...
-        setTimeout(drawLine, frameRateLeft, x, y, (distance - 1) * Math.random(), angle - 1, frameRate);
-        setTimeout(drawLine, frameRateRight, x, y, (distance - 1) * Math.random(), angle + 1, frameRate);
+        // add conditionals here, e.g., (if distance % 2 === 0 ). ...
+        setTimeout(drawLine, delayLeft, x, y, (distance - 1) * Math.random(), angle - 1, delayLeft);
+        setTimeout(drawLine, delayRight, x, y, (distance - 1) * Math.random(), angle + 1, delayRight);
     }
 };
 
 function start() {
-  for (var x = 0; x < canvas.width; x += Math.floor(Math.random() * 5)) {
+  var spread = rand(3, 10);
+  
+  for (var x = 0; x < canvas.width; x += rand(1, 5)) {
     var angleFactor = Math.random() * 10 - 5;
-    var frameRate = Math.random() * 800;
+    var delay = rand(200, 8000);
     
-    // start the animation
-    setTimeout(drawLine, framerate, x, canvas.height, Math.random() * 50, 0, angleFactor, frameRate);
+    // start the animation          /*-->         arguments for the drawLine function         <--*/
+    setTimeout(drawLine, delay, x, canvas.height, Math.random() * 50, 0, angleFactor, delay);
   }
+}
+
+// generate a random integer between two bounds
+function rand(lo, hi) {
+  return Math.floor((hi - lo) * Math.random()) + lo;
 }
 
 start();
