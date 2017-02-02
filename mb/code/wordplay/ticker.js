075ea9df577228f9987b756404694a8498630154
ggorlen <gsgorlen@gmail.com>
2017-02-01 20:05:22 -0800

    pre-class update

--- mb/code/wordplay/ticker.js
+++ mb/code/wordplay/ticker.js
@@ -1,71 +1,61 @@
-/* ticker: a simple text scroller */
+/* ticker: a simple text scroller
+ * constructo parameters: text to scroll, the size of t
+ * he box in characters, and the output DOM element id
+ */
+var Ticker = function(text, id, size, speed) {  
 
-// global interval and callback
-var interval;
-var update;
-var speed;
+  // abort constructor for invalid arguments
+  if (!text || !id || !document.getElementById(id)) {
+    throw new Error("Invalid argument(s)");
+  }
 
-// parameters: text to scroll, the size of the box 
-// in characters, and the output DOM element id
-function ticker(text, id, size, tickerSpeed) {  
+  // insterval instance variable
+  this.interval;
   
-  // validate input
-  if (text && id && document.getElementById(id)) {
-  
-    // assign parameter speed to global speed
-    speed = tickerSpeed;
-    
-    // provide default values for size and speed
-    if (!speed || speed < 1) {
-      speed = 250;
+  // provide default values for size and speed
+  if (!speed || speed < 1) {
+    speed = 250;
+  }
+  if (!size || size < 1) {
+    size = 10;
+  }
+
+  // variables to describe the text and  
+  // beginning and end points to display
+  var start = 0;
+  var end = size % text.length;
+   
+  // this function will be called to
+  // update the screen each frame
+  this.update = function() {
+
+    // conditional to help format wraparound
+    if (end < start) {
+      document.getElementById(id).innerHTML =
+        text.substring(start, text.length) +
+        text.substring(end - size, end);
     }
-    if (!size || size < 1) {
-      size = 10;
+    else {
+      document.getElementById(id).innerHTML =
+        text.substring(start, end);
     }
-
-    // variables to describe the text and  
-    // beginning and end points to display
-    var start = 0;
-    var end = size % text.length;
     
-    // this function will be called to 
-    // update the screen each frame
-    update = function () {
+    // update the start and end points.  
+    // % is the modulus (remainder) operator
+    // which allows the text to wrap.
+    start = ++start % text.length;
+    end = ++end % text.length;
+  };
 
-      // conditional to help format wraparound
-      if (end < start) {
-        document.getElementById(id).innerHTML = 
-          text.substring(start, text.length) + 
-          text.substring(end - size, end);
-      }
-      else {
-        document.getElementById(id).innerHTML = 
-          text.substring(start, end);
-      }
-      
-      // update the start and end points.  
-      // % is the modulus (remainder) operator 
-      // which allows the text to wrap.
-      start = ++start % text.length;
-      end = ++end % text.length;
-    };
-    
-    // start the interval, calling the update 
-    // function every 250 ms
-    interval = setInterval(update, speed);
-  }
-}
+  // pauses the ticker
+  this.pause = function() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  };
 
-// pauses the ticker
-function pause() {
-  if (interval) {
-    clearInterval(interval);
-  }
-}
-
-// resumes the paused ticker
-function resume() {
-  if (speed) {
-    interval = setInterval(update, speed);
-  }
-}
+  // starts the ticker
+  this.start = function() {
+    this.interval = setInterval(this.update, speed);
+  };
+};
