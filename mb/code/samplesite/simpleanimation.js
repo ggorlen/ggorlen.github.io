b0eff7a24cc7fbf33e6eacf143f571a316e5a35b
ggorlen <gsgorlen@gmail.com>
2017-03-07 16:53:43 -0800

    mid class update

--- mb/code/samplesite/simpleanimation.js
+++ mb/code/samplesite/simpleanimation.js
@@ -2,51 +2,93 @@
 let canvas = document.getElementById("mycanvas");
 let ctx = canvas.getContext("2d");
 
-// variables to represent the position
-let x = 50;
-let y = 50;
-let vx = 5;
-let vy = 5;
-let size = 100;
+// constants
+const NUM_BOXES = 100;
+
+// variables
+let boxes = [];
+
+// class to represent boxes
+let Box = function(x, y, vx, vy, size, color) {
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.size = size;
+  this.color = color;
+  
+  // collision detection for this box
+  this.collisionDetection = function() {
+  
+    // right wall
+    if (this.x + this.size >= canvas.width) {
+      this.x = canvas.width - this.size;
+      this.vx = -this.vx + rInt(-3, 3);
+    }
+    // left wall
+    if (this.x <= 0) {
+      this.x = 0;
+      this.vx = -this.vx + rInt(-3, 3);
+    }
+    // top
+    if (this.y + this.size >= canvas.height) {
+      this.y = canvas.height - this.size;
+      this.vy = -this.vy + rInt(-3, 3);
+    }
+    // bottom
+    if (this.y <= 0) {
+      this.y = 0;
+      this.vy = -this.vy + rInt(-3, 3);
+    }
+  };
+    
+  // draw this box
+  this.draw = function() {
+    ctx.fillStyle = "hsl(" + rInt(0, 255) + ", 100%, 50%)";
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  };
+  
+  // update position
+  this.move = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+};
+
+// function to set up the animation
+function init() {
+    
+  // populate the boxes array with box objects
+  for (let i = 0; i < NUM_BOXES; i++) {
+      
+    // create some properties for a box
+    let x = rInt(0, canvas.width);
+    let y = rInt(0, canvas.height); 
+    let vx = rInt(-5, 5);
+    let vy = rInt(-5, 5);
+    let size = rInt(1, 20);
+    let color = "hsl(" + rInt(0, 255) + ", 100%, 50%)";
+    
+    // make a new box
+    boxes.push(new Box(x, y, vx, vy, size, color));
+  }
+}
 
 // update function called each frame
 let update = function() {
-  collisionDetection();
-  
   // clear canvas
   ctx.clearRect(0,0,canvas.width,canvas.height);
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].collisionDetection();
+    boxes[i].move();
+    boxes[i].draw();
+  }
   
-  // draw a rectangle
-  ctx.fillStyle = "#000000";
-  ctx.fillRect(x, y, size, size);
-  
-  // update position
-  x += vx;
-  y += vy;
 };
 
-let collisionDetection = function() {
-  // right wall
-  if (x + size >= canvas.width) {
-    x = canvas.width - size;
-    vx = -vx + rInt(-3, 3);
-  }
-  // left wall
-  if (x <= 0) {
-    x = 0;
-    vx = -vx + rInt(-3, 3);
-  }
-  // top
-  if (y + size >= canvas.height) {
-    y = canvas.height - size;
-    vy = -vy + rInt(-3, 3);
-  }
-  // bottom
-  if (y <= 0) {
-    y = 0;
-    vy = -vy + rInt(-3, 3);
-  }
-};
+// call init!
+init();
 
 // set a timer to call our update function
 let interval = setInterval(update, 30);
