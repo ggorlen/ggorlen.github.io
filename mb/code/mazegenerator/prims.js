6e8a0f2af54964c37c7591dd93ac436af4292989
ggorlen <gsgorlen@gmail.com>
2017-04-25 15:23:44 -0700

    add prim's algorithm

--- mb/code/mazegenerator/prims.js
+++ mb/code/mazegenerator/prims.js
@@ -3,6 +3,10 @@
 
 /**
  * Prim's algorithm -- in progress!
+ *
+ * http://sebastianthomas.net/prims-algorithm.html
+ * http://weblog.jamisbuck.org/2011/1/10/maze-generation-prim-s-algorithm
+ * https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_Prim.27s_algorithm
  */
 let Prims = function() {};
 
@@ -12,41 +16,50 @@ Prims.prototype.carve = function(maze) {
   // Set of cells left to explore
   let frontier = [];
 
-  // Randomly select an unvisited cell from the grid and mark it visited
+  // Randomly select an unvisited cell from the grid and mark it part of the maze
   let cell = sample(grid);
+  cell.visited = true;
+
+  // Add this cell to the animation queue
+  animStates.push(["cell_" + cell.y + "_" + cell.x, cell]);
 
   // Add its neighbors to the frontier
   for (let dir in cell.neighbors) {
-    frontier.push(cell.neighbors[dir]);
+    if (cell.neighbors[dir]) {
+      frontier.push(cell.neighbors[dir]);
+    }
   }
 
   // Keep going while there are unvisited cells in the frontier
   while (frontier.length) {
 
     // Remove a random cell from the frontier
-    cell = frontier.splice(Math.floor(Math.random() * frontier.length), 1)[0];
-    if (!cell) break;
+    cell = randPop(frontier);
+
+    // Mark this cell as part of the maze
     cell.visited = true;
 
-    // Get this cell's neighbor keys in random order
+    // Carve a path between this cell and a cell already in the maze
     let randDirs = shuffle(Object.keys(cell.neighbors)); 
 
-    // Find a valid random neighbor and add it to the maze
-    for (let dir in randDirs) {
-      if (cell.neighbors[dir] && !cell.neighbors[dir].visited) {
-        let neighbor = cell.neighbors[dir];
-        neighbor.visited = true; //? ?
+    for (let i = 0; i < randDirs.length; i++) {
+      let neighbor = cell.neighbors[randDirs[i]];
+      if (neighbor && neighbor.visited) {
         cell.link(neighbor);
-
-        // Add its unvisited neighbors to the frontier
-        for (let nDir in neighbor.neighbors) {
-          if (neighbor.neighbors[nDir] && 
-              !neighbor.neighbors[nDir].visited) {
-            frontier.push(neighbor.neighbors[nDir]);
-          }
-        }
         break;
       }
     }
+
+    // Add this cell's neighbors to the frontier if they aren't already
+    // a member of the frontier or part of the maze
+    for (let dir in cell.neighbors) {
+      let neighbor = cell.neighbors[dir];
+      if (neighbor && frontier.indexOf(neighbor) < 0 && !neighbor.visited) {
+        frontier.push(neighbor);
+      }
+    }
+
+    // Add this cell to the animation queue
+    animStates.push(["cell_" + cell.y + "_" + cell.x, cell]);
   }
 }; // end carve
--- mb/code/mazegenerator/utility.js
+++ mb/code/mazegenerator/utility.js
@@ -15,5 +15,11 @@ function shuffle(arr) {
 
 // Randomly chooses an element from an array
 function sample(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.random() * arr.length | 0];
+} // end sample
+
+
+// Randomly deletes an element from an array
+function randPop(arr) {
+  return arr.splice((Math.random() * arr.length) | 0, 1)[0];
 } // end sample
