211e5ea7c203860cca99809bf282b8a298bebe3c
ggorlen <gsgorlen@gmail.com>
2018-09-26 20:52:42 -0700

    add bf email

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 "use strict";
 
-const height = 5;
-const width = 20;
+const height = 6;
+const width = 23;
 const palette = {
-  topLCorner: "\u250c",
-  topRCorner: "\u2510",
-  btmLCorner: "\u2514",
-  btmRCorner: "\u2518",
-  hLine: "\u2500",
-  vLine: "\u2502",
+  topLCorner: "\u2554",
+  topRCorner: "\u2557",
+  btmLCorner: "\u255a",
+  btmRCorner: "\u255d",
+  hLine: "\u2550",
+  vLine: "\u2551",
   shade: "\u2591"
 };
 
@@ -23,25 +23,25 @@ for (let j = 0; j < width; j++) {
 res.push(palette.topRCorner);
 res.push("<br>");
 
-for (let i = 0; i < 3; i++) {
-  res.push(palette.vLine);
-
-  if (i === 1) {
-    res.push("    hello world     ");
-  }
-  else {
-    res.push("                    ");
-  }
-
-  res.push(palette.vLine);
-
-  if (i > 0) {
-    res.push(palette.shade);
-    res.push(palette.shade);
-  }
-  
-  res.push("<br>");
-}
+[
+  " ".repeat(width),
+  "         hello!        ",
+  " ".repeat(width),
+  "  ++++++++++[>+++++++  ",
+  "  +++>+++++++++++>+++  ",
+  "  +++>+++++<<<<-]>+++  ",
+  "  .>+++++.<.>----.+++  ",
+  "  .<+++++.-------.>--  ",
+  "  --.>++++.<<++.>-.<-  ",
+  "  -----.>----.+++.>>-  ",
+  "  ---.<<<++.>+++.--.   ",
+  " ".repeat(width),
+].forEach(e => 
+  res.push(
+    palette.vLine + e + palette.vLine + 
+    palette.shade + palette.shade + "<br>"
+  )
+);
 
 res.push(palette.btmLCorner);
 
@@ -53,8 +53,8 @@ res.push(palette.btmRCorner);
 res.push(palette.shade);
 res.push(palette.shade);
 res.push("<br>");
-
 res.push("   ");
+
 for (let j = 0; j <= width; j++) {
   res.push(palette.shade);
 }
